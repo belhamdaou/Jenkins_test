0a40340aa8ffa7b7116c343884d83d12b81eb249
unknown <brahim.elhamdaoui@yahoo.de>
2015-10-03 21:03:50 +0200

    Quadrat class hinzugefügt

--- JenkinsBeispiel/src/main/java/de/philulay/jenkinsbeispiel/Main.java
+++ JenkinsBeispiel/src/main/java/de/philulay/jenkinsbeispiel/Main.java
@@ -8,13 +8,14 @@ public class Main {
 	public static void main(String[] args) {
 		Fakultaet faku = new Fakultaet();
 		Fibonacci fib = new Fibonacci();
-		
+		Quadrat sqrt = new Quadrat();
 		int[] zahlen1 = fib.rekursiv(10);
 		int[] zahlen2 = fib.iterativ(10);
 		
 		System.out.println("Fakultät von 5 iterativ: "+faku.iterativ(5));
 		System.out.println("Fakultät von 5 rekursiv: "+faku.rekursiv(5));
 		System.out.println("Fibonaccizahlen rekursiv: ");
+		System.out.println("Quadrat: "+sqrt.Zahlquadrat(2) );
 		
 		for(int i=0;i<10;i++){
 			System.out.println("      "+i+".: "+zahlen1[i]);
---
+++ JenkinsBeispiel/src/main/java/de/philulay/jenkinsbeispiel/Quadrat.java
@@ -0,0 +1,13 @@
+package de.philulay.jenkinsbeispiel;
+
+public class Quadrat {
+	
+	public int Zahlquadrat(int zahl){
+		
+		
+		
+		return (zahl * zahl);
+		
+	}
+
+}
